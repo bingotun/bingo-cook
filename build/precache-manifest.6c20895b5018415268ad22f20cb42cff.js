5744a694feb57534dd4f246c6861696eaeebcd32
bingobo <17642059382@163.com>
2020-05-18 10:13:51 +0800

    commit-1

---
+++ build/precache-manifest.6c20895b5018415268ad22f20cb42cff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2befe5c3d108ef56d39b1891ca7ed57e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42de5ce9a5e11af9a4de",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8ac79c399be222b435f",
+    "url": "/static/js/2.2be23bb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2be23bb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42de5ce9a5e11af9a4de",
+    "url": "/static/js/main.76a9c2a7.chunk.js"
+  },
+  {
+    "revision": "4fbc12cddc95a8e9bb83",
+    "url": "/static/js/runtime-main.f6b82a38.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
